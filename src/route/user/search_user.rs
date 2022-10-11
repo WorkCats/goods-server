9fbddc40adaa788413555ce9cecd5a41bfae45c2
夜白 <yb3412436097@gmail.com>
2022-10-11 22:50:03 +0800

    修改搜索所对应的权限

--- src/route/user/search_user.rs
+++ src/route/user/search_user.rs
@@ -3,17 +3,22 @@ use crate::claims::{claims_get_user};
 
 use crate::sql::sqlite_util::sql_connect;
 
-use crate::route::user::{create_user_list_result_claims_err, create_user_list_result_sql_connect_err, create_user_list_result_sql_err, create_user_list_success_result, UserListResult, UserName};
+use crate::route::user::{create_user_list_result_claims_err, create_user_list_result_sql_connect_err, create_user_list_result_sql_err, create_user_list_success_result, USER_LIST_RESULT_ADMINISTRATOR_ERRCODE, UserListResult, UserName};
 use crate::sql::user;
 
 
 pub async fn search_user(headers: HeaderMap, Json(user): Json<UserName>) -> Json<UserListResult> {
     return Json(match sql_connect().await {
         Ok(mut conn) => match claims_get_user(headers, &mut conn).await {
-            Ok(_) => match user::search_user(&mut conn, user.username).await {
-                Ok(user_list) => create_user_list_success_result(user_list),
-                Err(err) => create_user_list_result_sql_err(err)
+            Ok(login_user) => if login_user.is_administrator {
+                match user::search_user(&mut conn, user.username).await {
+                    Ok(user_list) => create_user_list_success_result(user_list),
+                    Err(err) => create_user_list_result_sql_err(err)
+                }
+            }else{
+                USER_LIST_RESULT_ADMINISTRATOR_ERRCODE.clone()
             }
+
 
             Err(errmsg) => create_user_list_result_claims_err(errmsg)
         }
