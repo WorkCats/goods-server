9732209607dd1ab38a78a1b478517a5cc2627dfd
夜白 <yb3412436097@gmail.com>
2022-10-11 22:40:08 +0800

    更新一个注释错误

--- src/route/good/del_good.rs
+++ src/route/good/del_good.rs
@@ -1,17 +1,22 @@
 use axum::{http::HeaderMap, Json};
+use serde::{Deserialize, Serialize};
 use crate::claims::claims_get_user;
 
 use crate::sql::{
     good::delete_good,
     sqlite_util::sql_connect,
 };
-use crate::route::{good::GoodName, create_text_result_claims_err, create_text_result_sql_err,  TEXT_SUCCESS_RESULT, TextResult, create_text_result_sql_connect_err};
 
+use crate::route::{create_text_result_claims_err, create_text_result_sql_err,  TEXT_SUCCESS_RESULT, TextResult, create_text_result_sql_connect_err};
 
-pub async fn del_good(headers: HeaderMap, Json(good): Json<GoodName>) -> Json<TextResult> {
+#[derive(Serialize, Deserialize)]
+pub struct GoodId {
+    good_id: String,
+}
+pub async fn del_good(headers: HeaderMap, Json(good): Json<GoodId>) -> Json<TextResult> {
     return Json(match sql_connect().await {
         Ok(mut conn) => match claims_get_user(headers, &mut conn).await {
-            Ok(_) => match delete_good(&mut conn, good.good_name).await {
+            Ok(_) => match delete_good(&mut conn, good.good_id).await {
                 Ok(_) => TEXT_SUCCESS_RESULT.clone(),
                 Err(err) => create_text_result_sql_err(err)
             }
