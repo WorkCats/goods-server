2540ed52aa27a65f3da5e7474321926492ddd6d2
夜白 <yb3412436097@gmail.com>
2022-10-12 09:51:51 +0800

    修改为相对路径

--- src/data.rs
+++ src/data.rs
@@ -7,7 +7,7 @@ pub static DOMAIN: &'static str = "0.0.0.0:3000";
 pub static DECODING_KEY: &'static str = "agoines";
 
 // SQL 对应的路径
-pub static SQL_FILE: &'static str = "/Users/agoines/Document/CLionProjects/goods-server/goods.sqlite";
+pub static SQL_FILE: &'static str = "./goods.sqlite";
 
 // 允许跨域的域名
 pub static ORIGIN:[&'static str; 2] = [
